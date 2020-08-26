7e43447ffaf10e77e0484afe0fa03f8f8d3bbe67
Davide Francavilla <davfrancavilla@gmail.com>
2020-08-26 16:24:26 +0200

    Esercizio terminato con bonus

--- js/script.js
+++ js/script.js
@@ -1,43 +1,55 @@
-var student = {
-    "nome" : "Mario",
-    "cognome" : "Rossi",
-    "eta" : "50"
-}
-
-for (var i in student) {
-    $("#first-student").append(i + ": " + student[i] + "<br>");
-}
-
-var students = [
-    {
-        "nome" : "Mauro",
-        "cognome" : "Bianchi",
-        "eta" : "55"
-    },
-    {
-        "nome" : "Maurizio",
-        "cognome" : "Verdi",
-        "eta" : "60"
-    },
-    {
-        "nome" : "Luca",
+$(document).ready(function(){
+    // creo un oggetto studente
+    var student = {
+        "nome" : "Mario",
         "cognome" : "Rossi",
-        "eta" : "65"
+        "eta" : "50"
     }
-]
-
-for (var i=0; i<students.length; i++) {
-    $("#nomes-list").append(students[i].nome + " " + students[i].cognome + "<br>");
-}
-
-student.nome = prompt("Inserisci il tuo nome");
-student.cognome = prompt("Inserisci il tuo cognome");
-student.eta = prompt("Inserisci la tua età");
-
-students.push(student);
-
-for (var i=0; i<students.length; i++){
-    for (var j in students[i]){
-        $("#array").append(j + ": " +students[i][j] + "<br>");
+    // stampo i dati dello studente appena creato
+    for (var i in student) {
+        $("#first-student").append(i + ": " + student[i] + "<br>");
     }
-}
+
+    // creo un array con diversi oggetti studenti
+    var students = [
+        {
+            "nome" : "Mauro",
+            "cognome" : "Bianchi",
+            "eta" : "55"
+        },
+        {
+            "nome" : "Maurizio",
+            "cognome" : "Verdi",
+            "eta" : "60"
+        },
+        {
+            "nome" : "Luca",
+            "cognome" : "Rossi",
+            "eta" : "65"
+        }
+    ]
+
+
+    //  stampo nome e cognome degli studenti presenti nell'array students
+    for (var i=0; i<students.length; i++) {
+        $("#names-list").append(students[i].nome + " " + students[i].cognome + "<br>");
+    }
+
+    // stampo tutti i dati degli studenti presenti nell'array students
+    for (var i=0; i<students.length; i++){
+        for (var j in students[i]){
+            $("#array").append(j + ": " +students[i][j] + "<br>");
+        }
+    }
+
+    // creo un quarto studente usando handlebars
+    var source = $('#entry-template').html();
+    var template = Handlebars.compile(source);
+    var student4 = {
+      "nome": prompt("Inserisci il tuo nome"),
+      "cognome": prompt("Inserisci il tuo cognome"),
+      "eta": prompt("Inserisci la tua età")
+    };
+    var html = template(student4);
+    $('#array').append(html);
+})
